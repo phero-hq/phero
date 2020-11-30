98cac7efd565c463e884edceb4512c49cc5ea0ca
kamilafsar <kamil@pressplay.dev>
2020-11-30 16:41:22 +0100

    export domain types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,4 @@
 export { default as generateClientSDK } from "./generateClientSDK"
 export { default as generateManifest } from "./generateManifest"
+export * from "./domain/manifest"
+export * from "./domain/JSValue"
