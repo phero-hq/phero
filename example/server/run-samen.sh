e6cb6bac71c333b745f854e0b5df13fe91e50ee7
kamilafsar <kamil@pressplay.dev>
2021-02-24 11:45:15 +0100

    add test script

---
+++ example/server/run-samen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+lerna bootstrap
+chmod +x ../server/node_modules/.bin/samen
+chmod +x ../client/node_modules/.bin/samen
+./node_modules/.bin/samen build
+./node_modules/.bin/samen buildClients
