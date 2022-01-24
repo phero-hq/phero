986d601230ddd6ce420769585af21e0701174423
Jasper Haggenburg <jasper@pressplay.dev>
2022-01-24 13:22:01 +0100

    delete example

--- example/server/run-samen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-lerna bootstrap
-chmod +x ../server/node_modules/.bin/samen
-chmod +x ../client/node_modules/.bin/samen
-./node_modules/.bin/samen build
--- example/server/src/calc.ts
+++
@@ -1,3 +0,0 @@
-export async function add(a: number, b: number): Promise<number> {
-  return a + b
-}
