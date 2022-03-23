ecbffaf72343a9ac7bc91de1df9ddc7e730249af
kamilafsar <kamil@pressplay.dev>
2022-03-23 15:01:31 +0100

    integrate

--- packages/ts-renderer/src/ts-elements/index.ts
+++ packages/ts-renderer/src/ts-elements/index.ts
@@ -2,51 +2,96 @@ import ts from "typescript"
 
 import { TSAny, TSAnyElement } from "./ts-any"
 import { TSArrowFunction, TSArrowFunctionElement } from "./ts-arrow-function"
-import { TSBinaryExpression } from "./ts-binary-expression"
+import {
+  TSBinaryExpression,
+  TSBinaryExpressionElement,
+} from "./ts-binary-expression"
 import { TSBlock, TSBlockElement } from "./ts-block"
+import { TSBoolean, TSBooleanElement } from "./ts-boolean"
 import { TSCallExpression, TSCallExpressionElement } from "./ts-call-expression"
 import { TSConst, TSConstElement } from "./ts-const"
-import { TSFalse } from "./ts-false"
+import { TSFalse, TSFalseElement } from "./ts-false"
 import { generateFunction, TSFunction, TSFunctionElement } from "./ts-function"
 import { TSIf, TSIfElement } from "./ts-if"
+import { TSImport, TSImportElement } from "./ts-import"
+import { TSLiteralType, TSLiteralTypeElement } from "./ts-literal-type"
+import { TSNull, TSNullElement } from "./ts-null"
+import { TSNumber, TSNumberElement } from "./ts-number"
+import { TSNumberLiteral, TSNumberLiteralElement } from "./ts-number-literal"
 import { TSParameter, TSParameterElement } from "./ts-parameter"
 import {
   TSPropertyAccessExpression,
   TSPropertyAccessExpressionElement,
 } from "./ts-property-access-expression"
 import { TSReturn, TSReturnElement } from "./ts-return"
+import { TSString, TSStringElement } from "./ts-string"
+import { TSStringLiteral, TSStringLiteralElement } from "./ts-string-literal"
+import { TSTrue, TSTrueElement } from "./ts-true"
 import { TSTypeReference, TSTypeReferenceElement } from "./ts-type-reference"
+import { TSUndefined, TSUndefinedElement } from "./ts-undefined"
 import { UnsupportedElementSupportedError } from "./utils"
 
 export type TSElements =
-  | TSAnyElement
-  | TSArrowFunctionElement
-  | TSConstElement
   | TSFunctionElement
   | TSParameterElement
-  | TSTypeReferenceElement
-  | TSBlockElement
+  | TSImportElement
+  | TSArrowFunctionElement
   | TSCallExpressionElement
-  | TSIfElement
+  | TSBinaryExpressionElement
   | TSPropertyAccessExpressionElement
+  | TSConstElement
+  | TSBlockElement
+  | TSIfElement
   | TSReturnElement
+  | TSFalseElement
+  | TSTrueElement
+  | TSTypeReferenceElement
+  | TSAnyElement
+  | TSBooleanElement
+  | TSNumberElement
+  | TSStringElement
+  | TSUndefinedElement
+  | TSLiteralTypeElement
+  | TSStringLiteralElement
+  | TSNumberLiteralElement
+  | TSNullElement
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
+      // declaration
       "ts-function": TSFunction
-      "ts-arrow-function": TSArrowFunction
       "ts-parameter": TSParameter
-      "ts-const": TSConst
-      "ts-any": TSAny
-      "ts-type-reference": TSTypeReference
-      "ts-block": TSBlock
+      "ts-import": TSImport
+
+      // expression
+      "ts-arrow-function": TSArrowFunction
       "ts-call-expression": TSCallExpression
-      "ts-if": TSIf
       "ts-binary-expression": TSBinaryExpression
       "ts-property-access-expression": TSPropertyAccessExpression
-      "ts-false": TSFalse
+
+      // statement
+      "ts-const": TSConst
+      "ts-block": TSBlock
+      "ts-if": TSIf
       "ts-return": TSReturn
+
+      // literal
+      "ts-false": TSFalse
+      "ts-true": TSTrue
+
+      // type-node
+      "ts-type-reference": TSTypeReference
+      "ts-any": TSAny
+      "ts-boolean": TSBoolean
+      "ts-number": TSNumber
+      "ts-string": TSString
+      "ts-undefined": TSUndefined
+      "ts-literal-type": TSLiteralType
+      // literals
+      "ts-string-literal": TSStringLiteral
+      "ts-number-literal": TSNumberLiteral
+      "ts-null": TSNull
     }
   }
 }
