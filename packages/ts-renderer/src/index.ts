f12a8bc5f143f9017428d1436002de097e837184
kamilafsar <kamil@pressplay.dev>
2022-04-02 13:49:53 +0200

    remove ts-renderer

--- packages/ts-renderer/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { renderAST } from "./renderer"
--- packages/ts-renderer/src/ts-elements/ts-root.ts
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { TSElements } from "."
-
-export interface TSRoot {
-  children: TSElements
-}
-
-export type TSRootElement = React.ReactElement<TSRoot, "ts-root">
