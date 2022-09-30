7fac11e4d64985396b00eda57c7063199cb8f9d7
kamilafsar <kamil@pressplay.dev>
2022-09-30 16:52:46 +0200

    upgrade to newest TS version and integrate eslint

---
+++ packages/core/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "standard-with-typescript",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json", // <-- Point to your project's tsconfig.json or create new one
+  },
+  // parser: '@typescript-eslint/parser',
+  rules: {
+    "deprecation/deprecation": "error",
+    "@typescript-eslint/comma-dangle": ["error", "always-multiline"],
+    "@typescript-eslint/quotes": ["error", "double"],
+    "@typescript-eslint/space-before-function-paren": ["error", "never"],
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+  },
+  plugins: ["deprecation"],
+}
