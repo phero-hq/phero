4e4a7daaee384aedba398557f28e47baa1c3cd6a
Jasper Haggenburg <jasper@pressplay.dev>
2023-01-19 17:07:01 +0100

    Get projects from workspace-settings

--- packages/cli/src/utils/getProjects.ts
+++ packages/cli/src/utils/getProjects.ts
@@ -3,52 +3,101 @@ import { promises as fs } from "fs"
 import { Project } from "../types"
 
 export default async function getProjects(): Promise<Project[]> {
-  const projects: (Project | undefined)[] = []
+  const projects = await getProjectsInternal()
+  return projects.sort((a, b) => (a.type === "client" ? -1 : 1))
+}
 
-  for (const rootFileName of await fs.readdir("./")) {
-    const rootFile = path.join(".", rootFileName)
-    if ((await fs.stat(rootFile)).isDirectory()) {
-      for (const nestedFileName of await fs.readdir(rootFile)) {
-        const nestedFile = path.join(rootFile, nestedFileName)
-        if ((await fs.stat(nestedFile)).isDirectory()) {
-          // Deeper projects not supported atm
-        } else {
-          projects.push(await getProject(nestedFile))
-        }
+async function getProjectsInternal(): Promise<Project[]> {
+  const projects: Project[] = []
+
+  // Get projects based on workspace-config
+  const workspacePaths = await getWorkspacePaths(".")
+  if (workspacePaths && workspacePaths.length > 0) {
+    for (const workspacePath of workspacePaths) {
+      const project = await getProject(workspacePath)
+      if (project) {
+        projects.push(project)
       }
-    } else {
-      projects.push(await getProject(rootFile))
     }
   }
+  if (projects.length > 0) {
+    return projects
+  }
 
+  // Get nested projects (1 level deep)
+  for (const fileName of await fs.readdir(".")) {
+    const filePath = path.join(".", fileName)
+    if ((await fs.stat(filePath)).isDirectory()) {
+      const nestedProject = await getProject(filePath)
+      if (nestedProject) {
+        projects.push(nestedProject)
+      }
+    }
+  }
+  if (projects.length > 0) {
+    return projects
+  }
+
+  // Get project at the root level
+  const rootProject = await getProject(".")
+  if (rootProject) {
+    projects.push(rootProject)
+  }
   return projects
-    .filter((p): p is Project => !!p)
-    .sort((a, b) => (a.type === "client" ? -1 : 1))
 }
 
-async function getProject(filePath: string): Promise<Project | undefined> {
-  if (!filePath.endsWith("package.json")) {
+interface PackageJson {
+  workspaces?: string[]
+  dependencies?: { [key: string]: string }
+  devDependencies?: { [key: string]: string }
+}
+
+async function getPackageJson(
+  dirPath: string,
+): Promise<PackageJson | undefined> {
+  const packagePath = path.join(dirPath, "package.json")
+  if (await exists(packagePath)) {
+    const packageString = await fs.readFile(packagePath, "utf-8")
+    return JSON.parse(packageString)
+  } else {
     return undefined
   }
+}
 
-  const packageString = await fs.readFile(filePath, "utf-8")
-  const packageJson = JSON.parse(packageString)
+async function getWorkspacePaths(
+  dirPath: string,
+): Promise<string[] | undefined> {
+  const packageJson = await getPackageJson(dirPath)
 
-  if (
-    packageJson.dependencies?.["@phero/client"] ||
-    packageJson.devDependencies?.["@phero/client"]
-  ) {
-    return { type: "client", path: formatProjectPath(filePath) }
-  }
-
-  if (
-    packageJson.dependencies?.["@phero/server"] ||
-    packageJson.devDependencies?.["@phero/server"]
-  ) {
-    return { type: "server", path: formatProjectPath(filePath) }
+  if (packageJson?.workspaces) {
+    // TODO: Glob
+    return packageJson.workspaces
   }
 }
 
-function formatProjectPath(packageJsonPath: string): string {
-  return path.relative(".", path.dirname(packageJsonPath))
+async function getProject(dirPath: string): Promise<Project | undefined> {
+  const packageJson = await getPackageJson(dirPath)
+
+  if (
+    packageJson?.dependencies?.["@phero/client"] ||
+    packageJson?.devDependencies?.["@phero/client"]
+  ) {
+    return { type: "client", path: dirPath }
+  }
+
+  if (
+    packageJson?.dependencies?.["@phero/server"] ||
+    packageJson?.devDependencies?.["@phero/server"]
+  ) {
+    return { type: "server", path: dirPath }
+  }
+}
+
+async function exists(path: string): Promise<boolean> {
+  try {
+    await fs.access(path)
+    return true
+  } catch {
+    return false
+  }
 }
