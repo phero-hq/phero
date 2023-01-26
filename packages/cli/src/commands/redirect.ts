cc03997f68204ce9448234675b195a1b99f04c10
Jasper Haggenburg <jasper@pressplay.dev>
2023-01-26 13:37:09 +0100

    Set detached to false

--- packages/cli/src/commands/redirect.ts
+++ packages/cli/src/commands/redirect.ts
@@ -3,7 +3,7 @@ import { spawn } from "child_process"
 export default function redirect(executable: string, argv: string[]) {
   spawn("npm", ["exec", "--", executable, ...argv], {
     cwd: process.cwd(),
-    detached: true,
+    detached: false,
     stdio: "inherit",
   })
 }
