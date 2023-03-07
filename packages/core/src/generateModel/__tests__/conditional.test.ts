03d250db2d2107b1bb39e0acdca2410f9b5bcc67
kamilafsar <kamil@pressplay.dev>
2023-03-07 22:48:53 +0100

    Fixes type parameters icw. conditional types

--- packages/core/src/generateModel/__tests__/conditional.test.ts
+++ packages/core/src/generateModel/__tests__/conditional.test.ts
@@ -559,7 +559,7 @@ describe("conditional", () => {
       }
       function test(): MyConditionalType<number, Hop<number>> { throw new Error() }
     `)
-
+    // console.log(JSON.stringify(modelMap, null, 4))
     expect(modelMap).toEqual({
       root: {
         type: "reference",
@@ -642,6 +642,343 @@ describe("conditional", () => {
                       type: "number",
                     },
                   ],
+                },
+              },
+            ],
+          },
+        },
+      },
+    })
+  })
+  test("MyConditionalType<boolean>", () => {
+    const modelMap = generateParserModelForReturnType(`
+      type MyConditionalType<T, K> = T extends string ? {
+        prop: number
+        x: K
+      } : {
+        prop: string
+        x: K
+      }
+      interface Hop<H> {
+        h: H
+      }
+      function test(): MyConditionalType<boolean, Hop<number>> { throw new Error() }
+    `)
+    // console.log(JSON.stringify(modelMap, null, 4))
+    expect(modelMap).toEqual({
+      root: {
+        type: "reference",
+        typeName: "MyConditionalType<boolean, Hop<number>>",
+        typeArguments: [
+          {
+            type: "boolean",
+          },
+          {
+            type: "reference",
+            typeName: "Hop<number>",
+            typeArguments: [
+              {
+                type: "number",
+              },
+            ],
+          },
+        ],
+      },
+      deps: {
+        "Hop<number>": {
+          type: "generic",
+          typeName: "Hop<number>",
+          typeArguments: [
+            {
+              type: "number",
+            },
+          ],
+          parser: {
+            type: "object",
+            members: [
+              {
+                type: "member",
+                name: "h",
+                optional: false,
+                parser: {
+                  type: "number",
+                },
+              },
+            ],
+          },
+        },
+        "MyConditionalType<boolean, Hop<number>>": {
+          type: "generic",
+          typeName: "MyConditionalType<boolean, Hop<number>>",
+          typeArguments: [
+            {
+              type: "boolean",
+            },
+            {
+              type: "reference",
+              typeName: "Hop<number>",
+              typeArguments: [
+                {
+                  type: "number",
+                },
+              ],
+            },
+          ],
+          parser: {
+            type: "object",
+            members: [
+              {
+                type: "member",
+                name: "prop",
+                optional: false,
+                parser: {
+                  type: "string",
+                },
+              },
+              {
+                type: "member",
+                name: "x",
+                optional: false,
+                parser: {
+                  type: "reference",
+                  typeName: "Hop<number>",
+                  typeArguments: [
+                    {
+                      type: "number",
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      },
+    })
+  })
+  test("a conditional type with or without a declared type should work the same", () => {
+    const modelMapA = generateParserModelForReturnType(`
+      type MyConditionalType<T, K> = T extends string ? RRR<number, K> : RRR<string, K>;
+      interface RRR<X, Y> {
+          prop: X;
+          x: Y;
+      }
+      interface Hop<H> {
+        h: H
+      }
+      function test(): MyConditionalType<boolean, Hop<number>> { throw new Error() }
+    `)
+
+    const modelMapB = generateParserModelForReturnType(`
+      type MyConditionalType<T, K> = T extends string ? {
+        prop: number
+        x: K
+      } : {
+        prop: string
+        x: K
+      }
+      interface Hop<H> {
+        h: H
+      }
+      function test(): MyConditionalType<boolean, Hop<number>> { throw new Error() }
+    `)
+
+    // console.log(JSON.stringify(modelMap, null, 4))
+    expect(modelMapA).toEqual(modelMapB)
+  })
+  test("ConditionalType with reference with default param", () => {
+    const modelMap = generateParserModelForReturnType(`
+      type MyConditionalType<K> = K extends string ? TheInterface : never
+
+      type TheInterface<T = boolean> = {
+        prop: T
+      }
+
+      function test(): MyConditionalType<string> { throw new Error() }
+    `)
+
+    // console.log(JSON.stringify(modelMap, null, 4))
+    expect(modelMap).toEqual({
+      root: {
+        type: "reference",
+        typeName: "MyConditionalType<string>",
+        typeArguments: [
+          {
+            type: "string",
+          },
+        ],
+      },
+      deps: {
+        "MyConditionalType<string>": {
+          type: "generic",
+          typeArguments: [
+            {
+              type: "string",
+            },
+          ],
+          typeName: "MyConditionalType<string>",
+          parser: {
+            type: "object",
+            members: [
+              {
+                type: "member",
+                name: "prop",
+                optional: false,
+                parser: {
+                  type: "boolean",
+                },
+              },
+            ],
+          },
+        },
+      },
+    })
+  })
+  test("ConditionalType with reference with given param", () => {
+    const modelMap = generateParserModelForReturnType(`
+      type MyConditionalType<K> = K extends string ? TheInterface<number> : never
+
+      interface TheInterface<T = boolean> {
+        prop: T
+      }
+
+      function test(): MyConditionalType<string> { throw new Error() }
+    `)
+
+    // console.log(JSON.stringify(modelMap, null, 4))
+    expect(modelMap).toEqual({
+      root: {
+        type: "reference",
+        typeName: "MyConditionalType<string>",
+        typeArguments: [
+          {
+            type: "string",
+          },
+        ],
+      },
+      deps: {
+        "MyConditionalType<string>": {
+          type: "generic",
+          typeArguments: [
+            {
+              type: "string",
+            },
+          ],
+          typeName: "MyConditionalType<string>",
+          parser: {
+            type: "object",
+            members: [
+              {
+                type: "member",
+                name: "prop",
+                optional: false,
+                parser: {
+                  type: "number",
+                },
+              },
+            ],
+          },
+        },
+      },
+    })
+  })
+
+  test("ConditionalType with reference with given param", () => {
+    const modelMap = generateParserModelForReturnType(`
+      type MyConditional<K, P = K extends string ? number : never> = {
+        prop: P
+      }
+
+      interface TheInterface<T = string> {
+        prop: T
+      }
+
+      function test(): MyConditional<string> { throw new Error() }
+    `)
+
+    // console.log(JSON.stringify(modelMap, null, 4))
+    expect(modelMap).toEqual({
+      root: {
+        type: "reference",
+        typeName: "MyConditional<string, number>",
+        typeArguments: [
+          {
+            type: "string",
+          },
+          {
+            type: "number",
+          },
+        ],
+      },
+      deps: {
+        "MyConditional<string, number>": {
+          type: "generic",
+          typeArguments: [
+            {
+              type: "string",
+            },
+            {
+              type: "number",
+            },
+          ],
+          typeName: "MyConditional<string, number>",
+          parser: {
+            type: "object",
+            members: [
+              {
+                type: "member",
+                name: "prop",
+                optional: false,
+                parser: {
+                  type: "number",
+                },
+              },
+            ],
+          },
+        },
+      },
+    })
+  })
+
+  test("ConditionalType with reference with given param", () => {
+    const modelMap = generateParserModelForReturnType(`
+      type MyConditional<K> = K extends string ? TheInterface<number> : never
+
+      interface TheInterface<T = string> {
+        prop: T
+      }
+
+      function test(): MyConditional<string> { throw new Error() }
+    `)
+
+    // console.log(JSON.stringify(modelMap, null, 4))
+    expect(modelMap).toEqual({
+      root: {
+        type: "reference",
+        typeName: "MyConditional<string>",
+        typeArguments: [
+          {
+            type: "string",
+          },
+        ],
+      },
+      deps: {
+        "MyConditional<string>": {
+          type: "generic",
+          typeArguments: [
+            {
+              type: "string",
+            },
+          ],
+          typeName: "MyConditional<string>",
+          parser: {
+            type: "object",
+            members: [
+              {
+                type: "member",
+                name: "prop",
+                optional: false,
+                parser: {
+                  type: "number",
                 },
               },
             ],
