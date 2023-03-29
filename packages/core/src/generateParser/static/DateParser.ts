9cd0c2f8f7970d0bac787e773e3608b6e34e9f63
kamilafsar <kamil@pressplay.dev>
2023-03-29 10:52:48 +0200

    fix regression in DateParser

--- packages/core/src/generateParser/static/DateParser.ts
+++ packages/core/src/generateParser/static/DateParser.ts
@@ -1,6 +1,9 @@
 import { type ParseResult } from "../../domain/Parser"
 
 export default function DateParser(data: unknown): ParseResult<Date> {
+  if (data instanceof Date) {
+    return { ok: true, result: data }
+  }
   if (
     typeof data === "string" &&
     /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/.test(data)
